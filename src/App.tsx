2920da3d1ffdd6d4aeff4356aef3b6257229d03c
noach844 <68166931+noach844@users.noreply.github.com>
2023-11-16 14:18:26 +0200

    secured routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { RequireAuth } from './components/RequiredAuth';
 import { Sign } from './pages/SignPage/Sign';
 
 function App() {
   return (
     <>
-      <Sign />
+      <Router>
+        <Routes>
+          <Route
+            path='/login'
+            element={
+              <>
+                <Sign />
+              </>
+            }
+          />
+          <Route
+            path='/'
+            element={
+              <RequireAuth>
+                <div>Home</div>
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </Router>
     </>
   );
 }
